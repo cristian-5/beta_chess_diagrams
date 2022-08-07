8e258a540332ed1d8c8cf7bfd3c18e1dff6a6b1b
Cristian-A <cristianantonuccio@icloud.com>
2022-08-07 15:08:30 +0200

    gifs

--- mod.ts
+++ mod.ts
@@ -1,6 +1,8 @@
 
 export * from 'https://esm.sh/svg2png-wasm@0.6.1';
-import { createCanvas, loadImage } from "https://deno.land/x/canvas@v1.4.1/mod.ts";
+import { createCanvas, loadImage } from 'https://deno.land/x/canvas@v1.4.1/mod.ts';
+import { encode, decode, ColorType, DecodeResult } from 'https://deno.land/x/pngs@0.1.1/mod.ts';
+import { GIFEncoder, quantize, applyPalette } from 'https://unpkg.com/gifenc@1.0.3/dist/gifenc.esm.js';
 
 import { PIECES_100, PIECES_50, BACKGROUND } from './pieces.ts';
 
@@ -22,17 +24,17 @@ export class Position {
 	}
 
 	place(piece: Piece, color: Color, square: string) {
-		if (square.length !== 2) return false;
-		if (square[0] < "a" || square[0] > "h") return false;
-		if (square[1] < "1" || square[1] > "8") return false;
+		if (square.length !== 2) return this;
+		if (square[0] < "a" || square[0] > "h") return this;
+		if (square[1] < "1" || square[1] > "8") return this;
 		this.squares[square] = { piece, color };
-		return true;
+		return this;
 	}
 
 	set(board: ({ type: string, color: Color } | null)[][]) {
-		if (board.length != 8) { this.clear(); return; }
+		if (board.length != 8) { this.clear(); return this; }
 		for (let i = 0; i < 8; i++) {
-			if (board[i].length != 8) { this.clear(); return; }
+			if (board[i].length != 8) { this.clear(); return this; }
 			for (let j = 0; j < 8; j++) {
 				const square = FILES[j] + (8 - i);
 				if (board[i][j] == null) {
@@ -43,25 +45,29 @@ export class Position {
 				this.squares[square] = { piece: type as Piece, color };
 			}
 		}
+		return this;
 	}
 
 	remove(square: string) {
-		if (square.length !== 2) return;
-		if (square[0] < "a" || square[0] > "h") return;
-		if (square[1] < "1" || square[1] > "8") return;
+		if (square.length !== 2) return this;
+		if (square[0] < "a" || square[0] > "h") return this;
+		if (square[1] < "1" || square[1] > "8") return this;
 		this.squares[square] = null;
+		return this;
 	}
 
 	clear() {
 		for (let i = "a"; i <= "h"; i = FILES[FILES.indexOf(i) + 1])
 			for (let j = 1; j <= 8; j++) this.squares[`${i}${j}`] = null;
+		return this;
 	}
 
 	highlight(square: string) {
-		if (square.length !== 2) return;
-		if (square[0] < "a" || square[0] > "h") return;
-		if (square[1] < "1" || square[1] > "8") return;
+		if (square.length !== 2) return this;
+		if (square[0] < "a" || square[0] > "h") return this;
+		if (square[1] < "1" || square[1] > "8") return this;
 		this.highlights.push(square);
+		return this;
 	}
 
 	async picture(perspective: Color = "w") {
@@ -159,4 +165,34 @@ export class Position {
 		return canvas.toBuffer("image/png");
 	}
 
+}
+
+function rgba(board: DecodeResult) {
+	if (board.colorType == ColorType.RGBA) return board.image.slice(0);
+	const data = new Uint8Array(Math.floor(board.image.length / 3) * 4);
+	for (let i = 0; i < board.image.length; i += 3) {
+		data.set([
+			board.image[i + 0],
+			board.image[i + 1],
+			board.image[i + 2],
+			255
+		], Math.floor(i / 3) * 4);
+	}
+	return data;
+}
+
+export function gif(frames: Uint8Array[]) {
+	if (frames.length == 0) return null;
+	let data = rgba(decode(frames[0]));
+	const palette = quantize(data, 30, { format: 'rgb444' });
+	let index = applyPalette(data, palette, 'rgb444');
+	const encoder = GIFEncoder();
+	encoder.writeFrame(index, 400, 400, { palette, repeat: -1 });
+	for (let i = 1; i < frames.length; i++) {
+		data = rgba(decode(frames[i]));
+		index = applyPalette(data, palette, 'rgb444');
+		encoder.writeFrame(index, 400, 400, { delay: 850, repeat: -1 });
+	}
+	encoder.finish();
+	return encoder.bytes();
 }
